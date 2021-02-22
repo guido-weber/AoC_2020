9e1c2685f727f6c406facbd1611f14503714f528
Guido Weber <guido.weber0815@googlemail.com>
2021-02-22 20:08:45 +0100

    Project setup

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject aoc-2020 "0.1.0-SNAPSHOT"
+  :description "Solutions for Advent of Code 2020"
+  :url "http://example.com/FIXME"
+  :license {:name "Unlicense"
+            :url "https://unlicense.org"}
+  :dependencies [[org.clojure/clojure "1.10.1"]]
+  :repl-options {:init-ns aoc-2020.core})
